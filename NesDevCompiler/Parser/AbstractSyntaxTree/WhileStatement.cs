b3fb857c21842c27fbc60656e1b1e137ff0587cb
Will Daras <willdaras@gmail.com>
2024-04-26 15:49:59 +0100

    parsing DONE

--- NesDevCompiler/Parser/AbstractSyntaxTree/WhileStatement.cs
+++ NesDevCompiler/Parser/AbstractSyntaxTree/WhileStatement.cs
@@ -1,12 +1,11 @@
 namespace NesDevCompiler.Parser.AbstractSyntaxTree;
 
-public class IfStatement : Statement
+public class WhileStatement : Statement
 {
 	public Expression Condition;
 	public List<Statement> Body = new List<Statement>();
-	public List<Statement>? ElseBody = null;
 
-	public IfStatement(Node parent, Expression condition) : base(parent)
+	public WhileStatement(Node parent, Expression condition) : base(parent)
 	{
 		Condition = condition;
 	}
